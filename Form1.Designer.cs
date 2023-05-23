227c7ace4feb845f2c65577d5c13fef07d4d8e1b
Kamva Cewu <Kamva@codeflux.co.za>
2023-05-23 20:11:00 +0200

    Added Components to the form designer

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,208 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            rdBlackCar = new RadioButton();
+            rdWhiteCar = new RadioButton();
+            rdBlackMini = new RadioButton();
+            rdWhiteMini = new RadioButton();
+            btnOrder = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            lblCarQ = new Label();
+            lblMinibusQ = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            lblMinibusLine = new Label();
+            label8 = new Label();
+            lblSpraybooth = new Label();
+            lblCarLine = new Label();
+            SuspendLayout();
+            // 
+            // rdBlackCar
+            // 
+            rdBlackCar.AutoSize = true;
+            rdBlackCar.Location = new Point(12, 17);
+            rdBlackCar.Name = "rdBlackCar";
+            rdBlackCar.Size = new Size(158, 29);
+            rdBlackCar.TabIndex = 0;
+            rdBlackCar.TabStop = true;
+            rdBlackCar.Text = "Black LUX 1000";
+            rdBlackCar.UseVisualStyleBackColor = true;
+            // 
+            // rdWhiteCar
+            // 
+            rdWhiteCar.AutoSize = true;
+            rdWhiteCar.Location = new Point(12, 52);
+            rdWhiteCar.Name = "rdWhiteCar";
+            rdWhiteCar.Size = new Size(164, 29);
+            rdWhiteCar.TabIndex = 1;
+            rdWhiteCar.TabStop = true;
+            rdWhiteCar.Text = "White LUX 1000";
+            rdWhiteCar.UseVisualStyleBackColor = true;
+            // 
+            // rdBlackMini
+            // 
+            rdBlackMini.AutoSize = true;
+            rdBlackMini.Location = new Point(12, 87);
+            rdBlackMini.Name = "rdBlackMini";
+            rdBlackMini.Size = new Size(139, 29);
+            rdBlackMini.TabIndex = 2;
+            rdBlackMini.TabStop = true;
+            rdBlackMini.Text = "Black MV500";
+            rdBlackMini.UseVisualStyleBackColor = true;
+            // 
+            // rdWhiteMini
+            // 
+            rdWhiteMini.AutoSize = true;
+            rdWhiteMini.Location = new Point(12, 122);
+            rdWhiteMini.Name = "rdWhiteMini";
+            rdWhiteMini.Size = new Size(145, 29);
+            rdWhiteMini.TabIndex = 3;
+            rdWhiteMini.TabStop = true;
+            rdWhiteMini.Text = "White MV500";
+            rdWhiteMini.UseVisualStyleBackColor = true;
+            // 
+            // btnOrder
+            // 
+            btnOrder.Location = new Point(12, 177);
+            btnOrder.Name = "btnOrder";
+            btnOrder.Size = new Size(141, 66);
+            btnOrder.TabIndex = 4;
+            btnOrder.Text = "Order";
+            btnOrder.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(247, 21);
+            label1.Name = "label1";
+            label1.Size = new Size(111, 25);
+            label1.TabIndex = 5;
+            label1.Text = "CAR QUEUE:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(563, 21);
+            label2.Name = "label2";
+            label2.Size = new Size(148, 25);
+            label2.TabIndex = 6;
+            label2.Text = "MINIBUS QUEUE:";
+            // 
+            // lblCarQ
+            // 
+            lblCarQ.AutoSize = true;
+            lblCarQ.Location = new Point(269, 58);
+            lblCarQ.Name = "lblCarQ";
+            lblCarQ.Size = new Size(59, 25);
+            lblCarQ.TabIndex = 7;
+            lblCarQ.Text = "label3";
+            // 
+            // lblMinibusQ
+            // 
+            lblMinibusQ.AutoSize = true;
+            lblMinibusQ.Location = new Point(597, 58);
+            lblMinibusQ.Name = "lblMinibusQ";
+            lblMinibusQ.Size = new Size(59, 25);
+            lblMinibusQ.TabIndex = 8;
+            lblMinibusQ.Text = "label4";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(327, 168);
+            label5.Name = "label5";
+            label5.Size = new Size(160, 25);
+            label5.TabIndex = 9;
+            label5.Text = "Car Assembly Line:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(327, 249);
+            label6.Name = "label6";
+            label6.Size = new Size(197, 25);
+            label6.TabIndex = 10;
+            label6.Text = "Minibus Assembly Line:";
+            // 
+            // lblMinibusLine
+            // 
+            lblMinibusLine.AutoSize = true;
+            lblMinibusLine.Location = new Point(371, 286);
+            lblMinibusLine.Name = "lblMinibusLine";
+            lblMinibusLine.Size = new Size(59, 25);
+            lblMinibusLine.TabIndex = 11;
+            lblMinibusLine.Text = "label7";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(355, 330);
+            label8.Name = "label8";
+            label8.Size = new Size(106, 25);
+            label8.TabIndex = 12;
+            label8.Text = "Spraybooth";
+            // 
+            // lblSpraybooth
+            // 
+            lblSpraybooth.AutoSize = true;
+            lblSpraybooth.Location = new Point(371, 371);
+            lblSpraybooth.Name = "lblSpraybooth";
+            lblSpraybooth.Size = new Size(59, 25);
+            lblSpraybooth.TabIndex = 13;
+            lblSpraybooth.Text = "label9";
+            // 
+            // lblCarLine
+            // 
+            lblCarLine.AutoSize = true;
+            lblCarLine.Location = new Point(371, 207);
+            lblCarLine.Name = "lblCarLine";
+            lblCarLine.Size = new Size(69, 25);
+            lblCarLine.TabIndex = 14;
+            lblCarLine.Text = "label10";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lblCarLine);
+            Controls.Add(lblSpraybooth);
+            Controls.Add(label8);
+            Controls.Add(lblMinibusLine);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(lblMinibusQ);
+            Controls.Add(lblCarQ);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(btnOrder);
+            Controls.Add(rdWhiteMini);
+            Controls.Add(rdBlackMini);
+            Controls.Add(rdWhiteCar);
+            Controls.Add(rdBlackCar);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private RadioButton rdBlackCar;
+        private RadioButton rdWhiteCar;
+        private RadioButton rdBlackMini;
+        private RadioButton rdWhiteMini;
+        private Button btnOrder;
+        private Label label1;
+        private Label label2;
+        private Label lblCarQ;
+        private Label lblMinibusQ;
+        private Label label5;
+        private Label label6;
+        private Label lblMinibusLine;
+        private Label label8;
+        private Label lblSpraybooth;
+        private Label lblCarLine;
     }
 }
